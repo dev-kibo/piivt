07387a583842e22be4b6e011705f91d66d950f69
Bojan Vujanović <bojan@ridgemaxllc.com>
2021-06-06 16:57:37 +0200

    Added common interfaces and setup router.

---
+++ 03-back-end/src/common/IErrorResponse.interface.ts
@@ -0,0 +1,4 @@
+export default interface IErrorResponse {
+  code: number;
+  description: string;
+}
--- 03-back-end/src/main.ts
+++ 03-back-end/src/main.ts
@@ -4,41 +4,61 @@ import * as cors from "cors";
 import * as fs from "fs";
 import * as path from "path";
 import * as morgan from "morgan";
-import AuthRouter from "./routers/auth.router";
-import IApplicationResources from "./services/IApplicationResources.inteface";
+import IApplicationResources from "./common/IApplicationResources.interface";
+import * as mysql2 from "mysql2/promise";
+import Router from "./router";
 
-const application: express.Application = express();
+async function main() {
+  const application: express.Application = express();
 
-fs.mkdirSync(path.dirname(Config.logger.path), {
-  mode: 0o755,
-  recursive: true,
-});
+  fs.mkdirSync(path.dirname(Config.logger.path), {
+    mode: 0o755,
+    recursive: true,
+  });
 
-application.use(
-  morgan(
-    ":date[iso]\t:remote-addr\t:method\t:status\t:res[content-length] bytes\t:response-time ms",
-    {
-      stream: fs.createWriteStream(Config.logger.path),
-    }
-  )
-);
+  application.use(
+    morgan(
+      ":date[iso]\t:remote-addr\t:method\t:status\t:res[content-length] bytes\t:response-time ms",
+      {
+        stream: fs.createWriteStream(Config.logger.path),
+      }
+    )
+  );
 
-application.use(
-  Config.server.static.route,
-  express.static(Config.server.static.path, {
-    cacheControl: Config.server.static.cacheControl,
-    dotfiles: Config.server.static.dotFiles,
-    etag: Config.server.static.etag,
-    maxAge: Config.server.static.maxAge,
-    index: Config.server.static.index,
-  })
-);
+  application.use(cors());
+  application.use(express.json());
 
-application.use(cors());
-application.use(express.json());
+  const resources: IApplicationResources = {
+    databaseConnection: await mysql2.createConnection({
+      host: Config.database.host,
+      port: Config.database.port,
+      user: Config.database.user,
+      password: Config.database.password,
+      database: Config.database.database,
+      charset: Config.database.charset,
+      timezone: Config.database.timezone,
+      supportBigNumbers: true,
+    }),
+  };
 
-const resources: IApplicationResources = {};
+  resources.databaseConnection.connect();
 
-AuthRouter.setupRoutes(application, resources);
+  resources.services = {};
 
-application.listen(Config.server.port);
+  application.use(
+    Config.server.static.route,
+    express.static(Config.server.static.path, {
+      cacheControl: Config.server.static.cacheControl,
+      dotfiles: Config.server.static.dotFiles,
+      etag: Config.server.static.etag,
+      maxAge: Config.server.static.maxAge,
+      index: Config.server.static.index,
+    })
+  );
+
+  Router.setupRoutes(application, resources, []);
+
+  application.listen(Config.server.port);
+}
+
+main();

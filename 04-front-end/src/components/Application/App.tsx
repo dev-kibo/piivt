e811ae297e94787a5e261d651681f964d8d3e65c
Bojan Vujanović <bojan@ridgemaxllc.com>
2021-06-11 19:41:27 +0200

    Setup React

---
+++ 04-front-end/src/components/Application/App.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './Application.sass';
+
+export default function App() {
+  return (
+    <div className="Application container">
+      <p>Test</p>
+    </div>
+  );
+}
